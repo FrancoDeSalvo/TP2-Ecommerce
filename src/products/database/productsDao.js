726ba45b79efab0ba9e18585c9734395760eba11
Franco <desalvo.fa@gmail.com>
2022-05-28 22:34:44 -0300

    correccion modo de persistencia

--- src/products/database/productsDao.js
+++ src/products/database/productsDao.js
@@ -1,16 +1,16 @@
-// import { MODO_PERSISTENCIA } from '../../config/config.js'
+ import { MODO_PERSISTENCIA } from '../../config/config.js'
 import * as daoMemory from './productsDaoMemory.js'
 import * as daoAnotherPeristence from './productsDaoAnotherPersistence.js'
 
 
-// let dao
+ let dao
 
-// switch (MODO_PERSISTENCIA) {
-//     case 'ARCHIVO':
-//         dao = daoAnotherPeristence
-//         break
-//     default:
-//         dao = daoMemory
-// }
+switch (MODO_PERSISTENCIA) {
+    case 'ARCHIVO':
+        dao = daoAnotherPeristence
+        break
+    default:
+        dao = daoMemory
+}
 
-export default daoMemory
+export default dao
--- src/users/database/usersDao.js
+++ src/users/database/usersDao.js
@@ -1,16 +1,16 @@
-// import { MODO_PERSISTENCIA } from '../../config/config.js'
+import { MODO_PERSISTENCIA } from '../../config/config.js'
 import * as daoMemory from './usersDaoMemory.js'
 import * as daoAnotherPeristence from './usersDaoAnotherPersistence.js'
 
 
-// let dao
+ let dao
 
-// switch (MODO_PERSISTENCIA) {
-//     case 'ARCHIVO':
-//         dao = daoAnotherPeristence
-//         break
-//     default:
-//         dao = daoMemory
-// }
+switch (MODO_PERSISTENCIA) {
+    case 'ARCHIVO':
+        dao = daoAnotherPeristence
+        break
+    default:
+        dao = daoMemory
+}
 
-export default daoMemory
+export default dao
